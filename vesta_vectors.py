b3d96aaa8e61807e5c01511e9f6dc1a5677f4c06
lucydot <lucywhalley@gmail.com>
2018-10-09 15:40:10 +0100

    start function to print out to file

--- vesta_vectors.py
+++ vesta_vectors.py
@@ -7,39 +7,50 @@ connecting atomic positions before and after an ionic relaxation
 import argparse
 import re
 
-# def read_in(settings):
+def read_in(settings):
 
-#     combined_positions = []
-#     combined_data = []
+	combined_positions = []
+	combined_data = []
 
-#     for filename in [settings.initial_filename, settings.final_filename]:
-#     	data = open(filename,'r').read()
-#     	combined_data.append(data)
+	for filename in [settings.filenames[0], settings.filenames[1]]:
+	    data = open(filename,'r').read()
+	    combined_data.append(data)
+	    
+	    struct_match=re.search(r"STRUC.*THERI",data,flags=re.S)[0]
+	    pos_match=re.findall(r"\d+\D+\s+\D+\d+\s+\d+\.\d+\s+(.*?)\s+\d+\D+\s+\d+",struct_match)
 
-# 		match = re.search("", data)
-#     	if match:
-#     		positions =  float(match.groups()[0])
-#    	 	else:
-#     		error("Invalid file format (I am only able to parse Vesta files)")
+	    if pos_match:
+	        positions = [[float(y) for y in x.split()] for x in pos_match]
+	    else:
+	        raise ValueError("Invalid file format (I am only able to parse Vesta files)")
 
-#     	positions = [[]]
-#     	combined_positions.append(positions)
 
-#     assert (len(combined_positions[0])==len(combined_positions[1]),"""different number of
-#     	atoms in initial and final files""")
+	    combined_positions.append(positions)
+    
+	data = {"initial_data": combined_data[0],
+	        "final_data": combined_data[1],
+	        "initial_positions": combined_positions[0],
+	        "final_positions": combined_positions[1],
+	        }
 
-#     data = {initial_data: combined_data[0];
-#     		final_data: combined_data[1];
-#     		initial_positions: combined_positions[0]; 
-#     		final_positions: combined_positions[1]}
+	for atom_id in sorted(settings.atoms_removed, reverse=True):
+	    del data["initial_positions"][atom_id-1]
+	    
+	for atom_id in sorted(settings.atoms_inserted, reverse=True):
+	    del data["final_positions"][atom_id-1]
 
-#     return data
+	assert len(combined_positions[0])==len(combined_positions[1]),"Unequal number of atoms before and after relaxation"
 
-# def calc_displacement(data):
+    return data
 
-# 	data["displacement"] = 
+def calc_displacement(data):
 
-# def print_to_file(data,settings):
+ 	displacement_frac = np.subtract(combined_positions[1],combined_positions[0])
+ 	displacement_angs = 
+ 	return displacement_angs
+ 	# what about non-cubic cells?
+
+def print_to_file(data,settings):
 
 
 def parse_args():
@@ -53,23 +64,29 @@ def parse_args():
 	parser.add_argument('--colour','-c',type=int, nargs=3,required=False,
 		default=[255,0,0])
 	parser.add_argument('--id','-i',type=int, nargs='+',required=False,
-		default=[0])
+		default=[])
 	parser.add_argument('--radius','-r',type=float, nargs=1, required=False,
 		default=1.0)
+	parser.add_argument('--atoms_removed','-ar',type=int,nargs='+',required=False,
+		default=[])
+	parser.add_argument('--atoms_inserted','-ai',type=int,nargs='+',required=False,
+		default=[])
+	parser.add_argument('--cutoff', '-x', type=float, nargs=1, required=False,
+		default = 0.05)
 	args = parser.parse_args()
 
 	return args
 
 if __name__=="__main__":
 
-#     print ("""Welcome to the distortion_vectors script.
-#     	   Type `distortion_vectors -h' to see command line options
-#     	   """)
+     print ("""Welcome to the vesta_vectors.
+     	   Type `vesta_vectors -h' to see the command line options
+    	   """)
  	settings = parse_args()
-# 	print ("Reading in data...")
-# 	data = read_in(settings)
-# 	print ("Calculating the displacements")
-# 	data["displacement"] = calc_displacement(data)
-# 	print ("Printing to file")
-# 	print_to_file(data,settings)
-# 	print ("All done.")
+ 	print ("Reading in data...")
+ 	data = read_in(settings)
+ 	print ("Calculating the displacements")
+ 	data["vectors"] = calc_displacement(data)
+ 	print ("Printing to file")
+	print_to_file(data,settings)
+	print ("All done.")
